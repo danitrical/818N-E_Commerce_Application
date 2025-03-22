0755cb6b53de4b62752702bf3a99a184a75d2e5f
danitrical <danishasim403@gmail.com>
2025-03-22 16:03:07 -0400

    feat: integrated aws-sdk for aws-secrets manager

--- includes/connect.php
+++ includes/connect.php
@@ -1,11 +1,11 @@
 <?php 
-// $con=mysqli_connect('localhost','root','','ecommerce_1');
-$con = new mysqli('localhost','root','','ecommerce_1');
+$dbHost = $_ENV['DB_HOST'];
+$dbUser = $_ENV['DB_USER'];
+$dbPass = $_ENV['DB_PASS'];
+$dbName = $_ENV['DB_NAME'];
+$mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
 if(!$con){
     die(mysqli_error($con));
 }
-
-
-
 
 ?>
